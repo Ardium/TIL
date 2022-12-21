ba8de98c7af7d93e29f2dff9e4b16f6a9c34b415
Ardium <ardiums@gmail.com>
2022-12-21 12:46:52 +0900

    게시판에 Bootstrap 효과 적용

--- VueJs/vue-toyproject-1/src/components/Notice/NoticeList.vue
+++ VueJs/vue-toyproject-1/src/components/Notice/NoticeList.vue
@@ -3,40 +3,40 @@
 <template>
     <div id="noticeList">
         <div id="notice_list_header">
-            <div id="notice_btngrp">
-                <button>등록</button>
-                <button>수정</button>
-                <button>삭제</button>
+            <div id="notice_btngrp" class="btn-group">
+                <button id="btn_register" class="btn btn-outline-dark">등록</button>
+                <button id="btn_modify" class="btn btn-outline-dark">수정</button>
+                <button id="btn_delete" class="btn btn-outline-dark">삭제</button>
             </div>
         </div>
         <div id="notice_list_body">
-            <table id="notice_table">
-                <thead>
+            <table id="notice_table" class="table table-hover">
+                <thead class="table-dark">
                     <tr>
-                        <th>순번</th>
-                        <th>카테고리</th>
-                        <th>제목</th>
-                        <th>작성자</th>
-                        <th>작성일자</th>
+                        <th scope="col">순번</th>
+                        <th scope="col">카테고리</th>
+                        <th scope="col">제목</th>
+                        <th scope="col">작성자</th>
+                        <th scope="col">작성일자</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>1</td>
+                        <td scope="row">1</td>
                         <td>공모전</td>
                         <td>제 3 회 Vue 공모전 개최 공지</td>
                         <td>관리자</td>
                         <td>2022/12/01</td>
                     </tr>
                     <tr>
-                        <td>2</td>
+                        <td scope="row">2</td>
                         <td>시스템 점검</td>
                         <td>2022/12/10 시스템 긴급 점검 공지</td>
                         <td>관리자</td>
                         <td>2022/12/07</td>
                     </tr>
                     <tr>
-                        <td>3</td>
+                        <td scope="row">3</td>
                         <td>시스템 점검</td>
                         <td>2022/12/31 시스템 긴급 점검 공지</td>
                         <td>관리자</td>
@@ -54,42 +54,25 @@ export default {}
 
 <style scoped>
 #noticeList {
+    padding: 5px;
     background-color: white;
 }
 
 #notice_btngrp {
+    margin: 0px 0px 5px 0px;
+
     float: right;
 }
 
 #notice_btngrp button {
-    margin: 5px;
-
-    font-size: x-small;
-}
-
-#notice_list_body {
-    margin: 5px;
-}
-
-#notice_list_body table {
-    width: 100%;
-
-    border: solid 1px black;
-    border-collapse: collapse;
+    font-size: 0.7em;
 }
 
 #notice_list_body table th, td {
     padding: 5px;
 
-    border-bottom: solid 1px black;
-
     text-align: center;
     font-family: 'Noto Sans KR', sans-serif;
-    font-size: x-small;
-}
-
-#notice_list_body table th {
-    background-color: gray;
-    color: white;
+    font-size: 0.7em;
 }
 </style>
--- VueJs/vue-toyproject-1/src/main.js
+++ VueJs/vue-toyproject-1/src/main.js
@@ -5,6 +5,11 @@ import App from './App.vue';
 
 import router from './router/index.js';
 
+import BootstrapVue from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 createApp(App)
 .use(router)
+.use(BootstrapVue)
 .mount('#app');
