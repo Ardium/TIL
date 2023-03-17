d57cf6c5f25937984e017193ed87935dc1ca2d5f
Ardium <ardiums@gmail.com>
2023-03-17 16:44:54 +0900

    Java / Q3929 (직사각형의 빈 좌표구하기) 풀이

--- CodingTest/Java/src/App.java
+++ CodingTest/Java/src/App.java
@@ -5,9 +5,10 @@ import programmers.*;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int n = 3;
-        System.out.println(Arrays.toString(new Java_1499().solution(n)));
+        int [][] v = {{1,4}, {3,4}, {3,10}};
+        System.out.println(new Java_3929().solution(v));
 
+        // System.out.println(Arrays.toString(new Java_1499().solution(n)));
         // new Java_7319().solution(n);
         // System.out.println(new Java_120811().solution(array));
         // System.out.println(new Java_120831().solution(n));
