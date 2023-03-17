76a96ac35a8312949b43c80080d0551f45e08b06
Ardium <ardiums@gmail.com>
2023-03-17 15:52:47 +0900

    Java / Q120811 (중앙값 구하기) 풀이

--- CodingTest/Java/src/App.java
+++ CodingTest/Java/src/App.java
@@ -3,8 +3,11 @@ import programmers.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int n = 10;
-        System.out.println(new Java_120831().solution(n));
+        int [] array = {1, 2, 7, 10, 11};
+        System.out.println(new Java_120811().solution(array));
+
+        // int n = 10;
+        // System.out.println(new Java_120831().solution(n));
 
         // int [] array = {7, 77, 17};
         // System.out.println(new Java_120912().solution(array));
