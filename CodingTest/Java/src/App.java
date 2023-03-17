124b447c5e0da2e9e5619e5876c9beda6ac80ac2
Ardium <ardiums@gmail.com>
2023-03-17 14:25:41 +0900

    Coding Test용 Java Project 생성

---
+++ CodingTest/Java/src/App.java
@@ -0,0 +1,8 @@
+import programmers.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int [] array = {7, 77, 17};
+        System.out.println(new Java120912().solution(array));
+    }
+}
