f9b217ca6d52ffd7bae34c84b5ac2ca565ce52e0
Ardium <ardiums@gmail.com>
2023-03-17 15:43:36 +0900

    Java / Q120831(짝수의 합) 풀이

--- CodingTest/Java/src/App.java
+++ CodingTest/Java/src/App.java
@@ -2,7 +2,11 @@ import programmers.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int [] array = {7, 77, 17};
-        System.out.println(new Java_120912().solution(array));
+
+        int n = 10;
+        System.out.println(new Java_120831().solution(n));
+
+        // int [] array = {7, 77, 17};
+        // System.out.println(new Java_120912().solution(array));
     }
 }
---
+++ CodingTest/Java/src/programmers/Java_120831.java
@@ -0,0 +1,21 @@
+package programmers;
+
+/*
+ * Question: 정수 n이 주어질 때, 
+ *           n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요. 
+ * Restrictions: 
+ *  * 0 < n ≤ 1000
+ * Input/Output:
+ *  * Case 1: Input = 10 → Output = 30 (2+4+6+8+10)
+ *  * Case 2: Input = 4 → Output = 6 (2+4)
+ */
+public class Java_120831 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 2; i <= n; i += 2) {
+            answer += i;
+        }
+        return answer;
+    }
+}
